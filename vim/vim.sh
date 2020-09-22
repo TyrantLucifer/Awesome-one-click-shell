a9c243c9900015ecb5b9aa194959df3aed7a5e95
TyrantLucifer <TyrantLucifer@gmail.com>
2020-09-22 16:20:59 +0800

    fix python release error

--- vim/vim.sh
+++ vim/vim.sh
@@ -51,7 +51,7 @@ function compile_vim(){
         --enable-multibyte \
         --enable-rubyinterp=yes \
         --enable-python3interp=yes \
-        --with-python3-config-dir=/usr/lib/$python3_release/$python3_config_dir \
+        --with-python3-config-dir=/usr/lib/$python3/$python3_config_dir \
         --enable-perlinterp=yes \
         --enable-luainterp=yes \
         --enable-gui=gtk2 \
